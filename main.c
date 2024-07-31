f2c749aea5f9430b301e3603d770ebb44b751169
lukesj07 <lukesjennings07@gmail.com>
2024-07-31 11:27:18 -0400

    add 2d map drawing and player drawing

--- main.c
+++ main.c
@@ -7,11 +7,11 @@
 #define MAP_WIDTH 20
 #define MAP_HEIGHT 20
 
-int map[MAP_WIDTH][MAP_HEIGHT] = {
+int map[MAP_HEIGHT][MAP_WIDTH] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -30,12 +30,14 @@ int map[MAP_WIDTH][MAP_HEIGHT] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
 
-struct PlayerState {
-    int position[3];
-    float direction[3];
+struct playerState {
+    float position[2];
+    float direction;
     int health;
 };
 
+void drawMap2D(int map[MAP_HEIGHT][MAP_WIDTH], SDL_Surface *surface);
+void drawPlayer(float pos[2], SDL_Surface *surface);
 
 int main() {
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -50,27 +52,60 @@ int main() {
         return -1;
     }
 
-    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+    SDL_Surface *surface = SDL_GetWindowSurface(window);
 
-    if(!window_surface) {
+    if(!surface) {
         printf("Failed to get the surface from the window\n");
         return -1;
     }
+
+    struct playerState player;
+    player.position[0] = SCREEN_WIDTH / 2;
+    player.position[1] = SCREEN_HEIGHT / 2;
+    player.direction = 0;
+    player.health = 200;
 
     int run = 1;
     while(run) {
         SDL_Event e;
         while(SDL_PollEvent(&e) > 0) {
-            switch(e.type) {
-                case SDL_QUIT:
-                    run = 0;
-                    break;
+            if (e.type == SDL_QUIT) {
+                run = 0;
             }
-        
-            SDL_UpdateWindowSurface(window);
         }
+
+        drawMap2D(map, surface);
+        drawPlayer(player.position, surface);
+        SDL_UpdateWindowSurface(window);
     }
 
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
 
+void drawMap2D(int map[MAP_HEIGHT][MAP_WIDTH], SDL_Surface *surface) {
+    int rectWidth = SCREEN_WIDTH / MAP_WIDTH;
+    int rectHeight = SCREEN_HEIGHT / MAP_HEIGHT;
+    SDL_Rect rect;
+    for (int i = 0; i < MAP_HEIGHT; i++) {
+        for (int j = 0; j < MAP_WIDTH; j++) {
+            if (map[i][j] == 1) {
+                rect.x = j * rectWidth;
+                rect.y = i * rectHeight;
+                rect.w = rectWidth - 1;
+                rect.h = rectHeight - 1;
+                SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format, 255, 255, 255));
+            }
+        }
+    }
+}
+
+void drawPlayer(float pos[2], SDL_Surface *surface) {
+    SDL_Rect rect;
+    rect.x = (int) pos[0];
+    rect.y = (int) pos[1];
+    rect.w = 10;
+    rect.h = 10;
+    SDL_FillRect(surface, &rect, SDL_MapRGB(surface->format, 255, 255, 0));
+}
