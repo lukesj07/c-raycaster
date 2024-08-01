1968e36fa313e13b2542dd22ee64ebf916844b47
CalebBunch <rcbcg123@gmail.com>
2024-08-01 19:07:27 -0400

    removed dead code

--- main.c
+++ main.c
@@ -17,14 +17,14 @@
 
 int map[MAP_HEIGHT][MAP_WIDTH] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -44,10 +44,7 @@ struct playerState {
     int health;
 };
 
-void drawMap2D(SDL_Renderer *renderer, int map[MAP_HEIGHT][MAP_WIDTH]);
-void drawPlayer2D(SDL_Renderer *renderer, float pos[2], float direction);
 void drawLine(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, SDL_Color color);
-void drawRays(SDL_Renderer *renderer, float pos[2], float direction);
 void render3DView(SDL_Renderer *renderer, float pos[2], float direction);
 float calculateDist(float p1[2], float p2[2]);
 
@@ -106,11 +103,7 @@ int main() {
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
-
-        //drawMap2D(renderer, map);
-        //drawPlayer2D(renderer, player.position, player.direction);
         render3DView(renderer, player.position, player.direction);
-
         SDL_RenderPresent(renderer);
     }
 
@@ -119,97 +112,6 @@ int main() {
     SDL_Quit();
     return 0;
 }
-
-void drawMap2D(SDL_Renderer *renderer, int map[MAP_HEIGHT][MAP_WIDTH]) {
-    int rectWidth = SCREEN_WIDTH / MAP_WIDTH;
-    int rectHeight = SCREEN_HEIGHT / MAP_HEIGHT;
-    SDL_Rect rect;
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    for (int i = 0; i < MAP_HEIGHT; i++) {
-        for (int j = 0; j < MAP_WIDTH; j++) {
-            if (map[i][j] == 1) {
-                rect.x = j * rectWidth;
-                rect.y = i * rectHeight;
-                rect.w = rectWidth - 1;
-                rect.h = rectHeight - 1;
-                SDL_RenderFillRect(renderer, &rect);
-            }
-        }
-    }
-}
-
-void drawPlayer2D(SDL_Renderer *renderer, float pos[2], float direction) {
-    SDL_Rect rect;
-    rect.x = (int) pos[0] - 5;
-    rect.y = (int) pos[1] - 5;
-    rect.w = 10;
-    rect.h = 10;
-    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-    SDL_RenderFillRect(renderer, &rect);
-
-    float dy = sin(direction) * 20;
-    float dx = cos(direction) * 20;
-    drawLine(renderer, pos[0], pos[1], pos[0] + dx, pos[1] + dy, (SDL_Color){255, 255, 0, 255});
-
-    drawRays(renderer, pos, direction);
-}
-
-void drawRays(SDL_Renderer *renderer, float pos[2], float direction) {
-    float mid[2] = {pos[0], pos[1]}; // Middle of player position
-
-    for (float a = direction - HALF_FOV; a <= direction + HALF_FOV; a += DEGREE) {
-        float rayAngle = fmod(a + 2 * PI, 2 * PI);  // Ensure angle is within 0 to 2PI
-
-        float verticalIntersection[2], horizontalIntersection[2];
-
-        // Calculate vertical intersection
-        calculateVerticalIntersection(mid, rayAngle, verticalIntersection);
-        while (1) {
-            int mapX = verticalIntersection[0] / TILE_SIZE;
-            int mapY = verticalIntersection[1] / TILE_SIZE;
-            if (mapX < 0 || mapX >= MAP_WIDTH || mapY < 0 || mapY >= MAP_HEIGHT) {
-                break;
-            }
-            if (map[mapY][mapX] == 1) {
-                break;
-            }
-            if (cos(rayAngle) > 0) {
-                verticalIntersection[0] += TILE_SIZE;
-                verticalIntersection[1] += TILE_SIZE * tan(rayAngle);
-            } else {
-                verticalIntersection[0] -= TILE_SIZE;
-                verticalIntersection[1] -= TILE_SIZE * tan(rayAngle);
-            }
-        }
-
-        // Calculate horizontal intersection
-        calculateHorizontalIntersection(mid, rayAngle, horizontalIntersection);
-        while (1) {
-            int mapX = horizontalIntersection[0] / TILE_SIZE;
-            int mapY = horizontalIntersection[1] / TILE_SIZE;
-            if (mapX < 0 || mapX >= MAP_WIDTH || mapY < 0 || mapY >= MAP_HEIGHT) {
-                break;
-            }
-            if (map[mapY][mapX] == 1) {
-                break;
-            }
-            if (sin(rayAngle) > 0) {
-                horizontalIntersection[1] += TILE_SIZE;
-                horizontalIntersection[0] += TILE_SIZE / tan(rayAngle);
-            } else {
-                horizontalIntersection[1] -= TILE_SIZE;
-                horizontalIntersection[0] -= TILE_SIZE / tan(rayAngle);
-            }
-        }
-
-        if (calculateDist(mid, verticalIntersection) < calculateDist(mid, horizontalIntersection)) {
-            drawLine(renderer, mid[0], mid[1], verticalIntersection[0], verticalIntersection[1], (SDL_Color){255, 0, 0, 255});
-        } else {
-            drawLine(renderer, mid[0], mid[1], horizontalIntersection[0], horizontalIntersection[1], (SDL_Color){255, 0, 0, 255});
-        }
-    }
-}
-
 
 //3D and helper functions
 void drawLine(SDL_Renderer *renderer, int x1, int y1, int x2, int y2, SDL_Color color) {
@@ -297,7 +199,7 @@ void render3DView(SDL_Renderer *renderer, float pos[2], float direction) {
             redVal = 255;
         } else {
             dist = calculateDist(pos, horizontalIntersection);
-            redVal = 200;
+            redVal = 150;
         }
 
         // fisheye fix
